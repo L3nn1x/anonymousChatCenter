7d13ff3adcd626056be1f6d467ce1d85aa12e890
Lennix o Robles <94206190+L3nn1x@users.noreply.github.com>
2022-04-30 14:32:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,119 @@
-import os
+
 import json
 from client import clientChat
-try:
+from server import serverChat
+import socket
+import os
+hostname = socket.gethostname()
 
-    os.system('sudo apt-get install python3')
-    os.system('sudo apt-get install pip')
-    os.system('sudo pip3 install colorama')
-    os.system('sudo pip3 install sucket')
-    os.system('sudo pip3 install threading')
-    os.system('sudo chmod +x client.py')
-    os.system('sudo chmod +x server.py')
-except Exception as e:
-    print(e)
-
-os.system('clear')
 from colorama import Fore, init
 init()
 
-fblue = Fore.BLUE
+
+fblue = Fore.LIGHTBLUE_EX
 fred = Fore.RED
 fgreen = Fore.GREEN
 fpurple = Fore.MAGENTA
 freset = Fore.RESET
 
-print(fpurple, """ █████╗  ██████╗ ██████╗
-██╔══██╗██╔════╝██╔════╝
-███████║██║     ██║     
-██╔══██║██║     ██║     
-██║  ██║╚██████╗╚██████╗
-╚═╝  ╚═╝ ╚═════╝ ╚═════╝           
-version: 1.0
-by: l3nn1x\n\n\n""", freset)
+numoptions = ['1', '2', '3', '4', '5', '6', '7']
+stroptions = ['set_IP', 'set_PORT', 'set_server_IP', 'set_server_PORT', 'run_client.py', 'run_server.py', 'quit']
+options = {}
+for index in range(len(numoptions)):
+    options[numoptions[index]] = stroptions[index]
+def selectOptions(select=str):
+    options = {}
+    for index in range(len(numoptions)):
+        options[numoptions[index]] = stroptions[index]
+
+    if select.isdigit():
+        if options[select] == 'run_server.py' or options[select] == 'run_client.py' or options[select] == 'quit':
+            return [select]
+
+
+        else:
+            selection = input(options[select]+': ')
+            return [select, selection]
+    else:
+        print('\n [*] please make an selection\n')
+
+
+def makeAction(command=list):
+    updateValue = {}
+
+    if options[command[0]] == stroptions[0]: # client IP configuration
+        with open('clientConf.json', 'r') as f:
+            updateValue = json.load(f)
+            updateValue['client-host'] = command[1]
+        with open('clientConf.json', 'w') as f:
+            json.dump(updateValue, f)
+    elif options[command[0]] == stroptions[1]: # client PORT configuration
+        with open('clientConf.json', 'r') as f:
+            updateValue = json.load(f)
+            updateValue['client-port'] = command[1]
+        with open('clientConf.json', 'w') as f:
+            json.dump(updateValue, f)
+
+    elif options[command[0]] == stroptions[2]: # server IP configuration
+        with open('serverConf.json', 'r') as f:
+            updateValue = json.load(f)
+            updateValue['server-host'] = command[1]
+        with open('serverConf.json', 'w') as f:
+            json.dump(updateValue, f)
+    elif options[command[0]] == stroptions[3]: # server PORT configuration
+        with open('serverConf.json', 'r') as f:
+            updateValue = json.load(f)
+            updateValue['server-port'] = command[1]
+        with open('serverConf.json', 'w') as f:
+            json.dump(updateValue, f)
+
+    elif options[command[0]] == stroptions[4]: # run client.py
+        with open('clientConf.json', 'r') as f:
+            config = json.load(f)
+            clientChat(host=config['client-host'], port=config['client-port'])
+    elif options[command[0]] == stroptions[5]: # run server.py
+        with open('serverConf.json', 'r') as f:
+            serCon = json.load(f)
+            serverChat(host=serCon['server-host'], port=serCon['server-port'])
+
+
+
+
+
+
+logo = """\t\t █████╗  ██████╗ ██████╗
+\t\t██╔══██╗██╔════╝██╔════╝
+\t\t███████║██║     ██║     
+\t\t██╔══██║██║     ██║     
+\t\t██║  ██║╚██████╗╚██████╗
+\t\t╚═╝  ╚═╝ ╚═════╝ ╚═════╝  """
+
+print(fpurple, logo, fred,"""         
+\t\tversion: 1.0
+\t\tby: l3nn1x\n\n\n""", freset)
 cliCon = {}
 
-with open('clientConf.json', 'r') as f:
-    cliCon = json.load(f)
-clientChat(host=cliCon['server-host'], port=cliCon['server-port'])
+for index in range(len(numoptions)):
+    print(fgreen, f'\t\t::[{numoptions[index]}]::', freset, fblue, stroptions[index], freset, '\n')
+
+while True:
+    try:
+        if os.name == 'nt':
+            os.system('cls')
+        else:
+            os.system('clear')
+
+        print(fpurple, logo, fred, """         
+        \t\tversion: 1.0
+        \t\tby: l3nn1x\n\n\n""", freset)
+
+        for index in range(len(numoptions)):
+            print(fgreen, f'\t\t::[{numoptions[index]}]::', freset, fblue, stroptions[index], freset, '\n')
+
+        user = input(f'\n::[{hostname}]:: make a selection: ', )
+        if options[user] == 'quit':
+            break
+        selOp = selectOptions(user)
+        makeAction(selOp)
+    except:
+        continue
