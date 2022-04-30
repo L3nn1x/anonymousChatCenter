d257b572afa93219f0d01dc0d3e2037b86aa7b51
Lennix o Robles <94206190+L3nn1x@users.noreply.github.com>
2022-04-30 14:37:56 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+
+def main():
+
+    try:
+        if os.name in 'nt':
+            os.system('python.exe -m pip install --upgrade pip')
+            os.system('pip3 install colorama')
+            os.system('pip3 install sucket')
+            os.system('pip3 install threading')
+
+
+        else:
+            os.system('sudo apt-get install python3')
+            os.system('sudo apt-get install pip')
+            os.system('sudo pip3 install colorama')
+            os.system('sudo pip3 install sucket')
+            os.system('sudo pip3 install threading')
+
+
+    except Exception as e:
+        print(e)
+
+if __name__ == '__main__':
+    main()
